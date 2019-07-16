9927d8e49076101434e143f85fb9b08e691c2ae1
スティーブンさん <underyourcover@icloud.com>
2019-07-16 14:57:53 -0700

    refactored req.pipe

--- proxy/index.js
+++ proxy/index.js
@@ -17,37 +17,37 @@ app.use(express.static(path.join(__dirname, '../dist')));
 // music-player-service
 app.use('/api/songs/:id', (req, res) => {
   const url = `http://localhost:3003/api/songs/${req.params.id}`;
-  req.pipe(request(url)).pipe(res);
+  request(url).pipe(res);
 });
 
 // comments-service: get comments
 app.use('/comments/init', (req, res) => {
   const url = `http://localhost:3001/comments/init`;
-  req.pipe(request(url)).pipe(res);
+  request(url).pipe(res);
 });
 
 // comments-service: post comments
 app.use('/comments/new', (req, res) => {
   const url = `http://localhost:3001/comments/new`;
-  req.pipe(request(url)).pipe(res);
+  request(url).pipe(res);
 });
 
 // sidebar-service: tracks
 app.use('/api/track/:id', (req, res) => {
   const url = `http://localhost:3002/api/track/${req.params.id}`;
-  req.pipe(request(url)).pipe(res);
+  request(url).pipe(res);
 });
 
 // sidebar-service: users
 app.use('/api/user/:user_name', (req, res) => {
   const url = `http://localhost:3002/api/user/${req.params.user_name}`;
-  req.pipe(request(url)).pipe(res);
+  request(url).pipe(res);
 });
 
 // sidebar-service: likes
 app.use('/api/track/likes/:track_id', (req, res) => {
   const url = `http://localhost:3002/api/track/likes/${req.params.track_id}`;
-  req.pipe(request(url)).pipe(res);
+  request(url).pipe(res);
 });
 
 const PORT = process.env.PORT || 3000;
